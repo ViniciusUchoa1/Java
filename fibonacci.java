956c0de2e2eb3f889bd76f3374cbc5d9876c09e8
ViniciusUchoa1 <42256667+ViniciusUchoa1@users.noreply.github.com>
2018-09-21 09:59:18 -0300

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
 
 public class Fibonacci {
+
     public static void main(String[] args) {
-        Scanner tcl = new Scanner(System.in); 
-         int num1 = 1, num2 = 1;
-         System.out.println("Digite o tamanho do vetor; ");
-         int a = tcl.nextInt();
-         int vetor [] = new int[a];
+    Scanner tcl = new Scanner(System.in);
+    System.out.println("Digite o tamanho do seu vetor");
+    int tamanho = tcl.nextInt();    
+    long vetor[] = new long[tamanho];
+    
+        for (int i = 0; i < vetor.length; i++) {
+           if (i < 2){
+               vetor[i] = i;
+           }else{
+               vetor[i] = vetor[i - 1] + vetor[i - 2];
+           }
+            
+        }
         
-        System.out.println(num2);
-        System.out.println(num1);
-        
-        for(int i = 1; i < vetor.length; i++){
-            num1 = num1 + num2;
-            num2 = num1 - num2;
-            System.out.println(num1);
+        System.out.println("Digite o número que deseja procurar");
+        int num = tcl.nextInt();
+        for (int j = 0; j < vetor.length; j++) {
+            if(num == j) {
+               System.out.println("O " + j +"° número da sequência é :"+vetor[j] ) ;
+                
+            }    
         }
     }
 }
